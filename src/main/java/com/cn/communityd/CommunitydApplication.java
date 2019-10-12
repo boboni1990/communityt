9bb9c6e40865b3adcefedfb3f3d76059b47cfc79
boboni1990 <17788734499@163.com>
2019-10-12 15:35:01 +0800

    fffffffffff

---
+++ src/main/java/com/cn/communityd/CommunitydApplication.java
@@ -0,0 +1,13 @@
+package com.cn.communityd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommunitydApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CommunitydApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cn/communityd/CommunitydApplicationTests.java
@@ -0,0 +1,16 @@
+package com.cn.communityd;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CommunitydApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
